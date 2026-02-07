39d36daef2cbfd3f66a706cdd9d818c65b0fdae9
Poly0n <adrian2005mag@hotmail.com>
2026-02-07 00:25:45 -0500

    Update SignatureChecker.h

--- WinGuard/include/SignatureChecker.h
+++ WinGuard/include/SignatureChecker.h
@@ -2,13 +2,10 @@
 #include "ProcessEnumerator.h"
 #include <unordered_set>
 #include <unordered_map>
-#include <fstream>
 #include <WinTrust.h>
 #include <winternl.h>
 #include <SoftPub.h>
 #include <filesystem>
-#include <sddl.h>
-#include <thread>
 #include "Logger.h"
 
 #pragma comment(lib, "wintrust.lib")
@@ -29,4 +26,5 @@ private:
 	bool isDirectoryUserWritable(const std::wstring& filePath);
 	bool getModules(DWORD pid, ProcessEnumerator& proc, std::unordered_map<DWORD, ProcessEnumerator::ProcessInformation>& processSnapshot);	
 };
+
 
